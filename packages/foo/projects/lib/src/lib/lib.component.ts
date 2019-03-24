51552ceff801921c0515e940f6eab7e879cafd1e
SergeyBzenko <sergey.bzenko@bhnetwork.com>
2019-03-24 20:50:20 +0200

    ss

---
+++ packages/foo/projects/lib/src/lib/lib.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'enl-lib',
+  template: `
+    <p>
+      lib wohrks!s
+    </p>
+  `,
+  styles: []
+})
+export class LibComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
